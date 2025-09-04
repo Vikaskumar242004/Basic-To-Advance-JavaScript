c8fdb00bea954d7b485e7e1af3ce1faf978f87ff
Vikas <167329026+Vikaskumar242004@users.noreply.github.com>
2025-09-04 11:37:21 +0530

    Add files via upload
    adding folder upto 4

---
+++ video 3/script.js
@@ -0,0 +1,38 @@
+console.log("VIKAHARO")
+
+let age = 17;
+let grace = 2;
+// age += grace  // x = x+y; and so on
+ 
+// console.log(age+grace);
+// console.log(age-grace);
+// console.log(age*grace);
+// console.log(age/grace);
+// console.log(age%grace);
+// console.log(age**grace); // ye square kr dega 
+
+
+
+if((age+  grace) >18){
+    console.log("you can drive")
+}
+else{
+    console.log("you cannot drive")
+}
+
+
+//  === operator which is used to  check the type and value 
+
+//  for example
+
+// "3" == 3; in js engine it gives true because == operator check only values
+
+// "3" === 3; and this conditon it gives false because they both check type and value
+
+
+a = 6;
+b = 8;
+
+let c = a>b ? (a-b):(a+b);
+console.log(c);
+
---
+++ video 4/script.js
@@ -0,0 +1,47 @@
+// console.log("video on loops")
+
+
+// for(let i =0; i<=10; i++){
+//     console.log(i)
+// }
+
+
+
+// for in loop
+// let obj = {
+//     name : "Vikas",
+//     role : "Programmer", company : "VIKAHARO's"
+// }
+
+
+
+// for(const key in obj){
+//     const element = obj[key]
+//     console.log(key,element)
+// }
+
+
+
+// for of loop 
+
+// for(const c of "Vikas"){
+//     console.log(c)
+// }
+
+
+// while loop
+// let i =0;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+
+// do while loop
+
+
+let i =10;
+do{
+    console.log(i)
+    i++;
+}while (i<6);
