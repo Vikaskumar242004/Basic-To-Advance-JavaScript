503fa9aed482fa9de88dcaf853ef57918b12b46d
Vikas <167329026+Vikaskumar242004@users.noreply.github.com>
2025-09-10 20:28:51 +0530

    adding files

---
+++ video 10/index.js
@@ -0,0 +1,57 @@
+let arr = [1,2,5,7,8]
+let arr1 = [1,2,5,6,2]
+let arr2 = [1,2,8,4,8]
+
+// console.log(arr)
+// console.log(arr.length)
+
+
+// for string
+// console.log(arr.toString())
+
+// arr.join use to join the two arrays by giving a word
+// console.log(arr.join(" and "))
+
+// array ke last ka element nikal lo or return kardo
+// console.log(arr.pop())
+
+// array ki updated length btata he
+// console.log(arr.push(100))
+
+
+// unshift push  ka bhai but ye first element me add krta he
+// console.log(arr.unshift(90));
+
+// shift pop ka bhai but ye phle element nikalta he 
+// console.log(arr.shift())
+
+
+// multiple arrays ko single array me convert kr dega
+// console.log(arr.concat(arr1,arr2))
+
+// used to sort the array
+// console.log(arr1.sort())
+
+// agar only splice likhenge to jitni length di hogi utne element nikal ke display kr dega
+// or main use iska add krne ke liye hota he phle jo remove krna he uska index dedo
+// or fir  baad me add krane wale number dedo us length pr
+// console.log(arr.splice(1,3,454,84))
+
+// or splice krne ke baad me array print krvaoge  to delete kre hue element nahi dekhenge bache hue dekhenge
+// console.log(arr)
+
+// its meaning that jonsa index diya he slice me usse start karo or array ki last length tk jao or  ek new array bnado
+// console.log(arr.slice(2));
+
+// 1 se leke 3 index tk pr isme 3 index include nahi hoga
+// console.log(arr.slice(1,3));
+
+
+// this is  used to reverse the array
+// console.log(arr.reverse())
+
+
+// for each loop
+// arr.forEach((Value, index, arr)=>{
+// console.log(value, index, arr)
+// })
---
+++ video 10/mfr.js
@@ -0,0 +1,17 @@
+let arr = [1,2,13,56,8];
+// let newarr = []
+// for(let i =0; i<arr.length; i++){
+//     const element = arr[i];
+//     newarr.push(element**2)
+// }
+
+
+// this is map()
+// let newarr = arr.map((e) =>{
+//     return e**2;
+// })
+// console.log(newarr)
+
+ 
+
+
