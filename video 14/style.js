035176dabc6a9a566e1f6bdab657cd1d80b65737
Vikas <167329026+Vikaskumar242004@users.noreply.github.com>
2025-09-12 22:54:18 +0530

    adding files
    upto folder 14

---
+++ video 14/style.js
@@ -0,0 +1,15 @@
+console.log("hello world")
+
+
+// by using this text we can determine the any part of the element in the code
+// in this case some chances that  we get the text when we want the first child but to remove it the another method is
+// document.body.childNodes[1].childNodes
+
+// by using this it only give the element child and ignore the text child 
+// document.body.firstElementchild
+
+
+
+//  and many more things about on the DOM'S in the documentation 
+
+/* like tags about table for the particular row and many more tags */
