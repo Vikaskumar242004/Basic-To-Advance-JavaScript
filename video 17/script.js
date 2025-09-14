ce78188ca5016d497c84b5bac1cfea5ece5a3a7f
Vikas <167329026+Vikaskumar242004@users.noreply.github.com>
2025-09-14 15:16:21 +0530

    adding files

---
+++ video 17/script.js
@@ -0,0 +1,7 @@
+console.log("Hello VIKAHARO's")
+
+document.getElementById("aa").style.backgroundColor = "red"
+document.getElementById("a").style.backgroundColor = "green"
+document.getElementById("b").style.backgroundColor = "yellow"
+document.getElementById("c").style.backgroundColor = "pink"
+document.getElementById("d").style.backgroundColor = "cyan"
